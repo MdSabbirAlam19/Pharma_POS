26090210bc3a5faebd78247c8620a122447de8a3
Md Sabbir Alam <mdsabbir77682@gmail.com>
2026-05-26 16:57:56 +0600

    fix indentation

--- app.py
+++ app.py
@@ -1249,7 +1249,7 @@ if __name__ == '__main__':
     print("   Admin: admin@citymedical.com / admin123")
     print("   Cashier: cashier@citymedical.com / cashier123")
     # app.run(debug=True, host='0.0.0.0', port=5000)
-    if __name__ == "__main__":
+if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
